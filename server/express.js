c25b6278f319c273c5e552adae4df33dcf43a3ee
zenjduke <zennajduke@gmail.com>
2018-09-13 10:53:35 -0500

    2

--- server/express.js
+++ server/express.js
@@ -17,7 +17,7 @@ const app = express()
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, '../build/static')))
+	app.use(express.static(path.join(__dirname, '../build')))
 	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname, '../build/index.html'))
 	})
