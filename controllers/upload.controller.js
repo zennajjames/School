f45485537d4611afdf7e9a1003bbb9dbdb1acf71
zenjduke <zennajduke@gmail.com>
2019-02-07 20:41:15 -0600

    aws-controller

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,21 +1,45 @@
 const upload = (req, res, next) => {
-    var s3Bucket = new AWS.S3();
+  var aws = require('aws-sdk'); 
+  require('dotenv').config(); // Configure dotenv to load in the .env file
+  // Configure aws with your accessKeyId and your secretAccessKey
+  aws.config.update({
+    region: 'us-east-1', // Put your aws region here
+    accessKeyId: process.env.AWSAccessKeyId,
+    secretAccessKey: process.env.AWSSecretKey
+  })
 
-    var s3 = new Router({ mergeParams: true });
-
-    var params = {
-        Bucket: 'BUCKET_NAME', // add your s3 bucket name here
-        Key: data.filename,
-        Expires: 60,
-        ContentType: data.filetype
+  const S3_BUCKET = process.env.bucket
+  // Now lets export this function so we can call it from somewhere else
+  exports.sign_s3 = (req,res) => {
+    const s3 = new aws.S3();  // Create a new instance of S3
+    const fileName = req.body.fileName;
+    const fileType = req.body.fileType;
+  // Set up the payload of what we are sending to the S3 api
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: fileName,
+      Expires: 500,
+      ContentType: fileType,
+      ACL: 'public-read'
     };
-
-    s3Bucket.getSignedUrl('putObject', params, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data);           // successful response
-        });
-    }
+  // Make a request to the S3 API to get a signed URL which we can use to upload our file
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+      if(err){
+        console.log(err);
+        res.json({success: false, error: err})
+      }
+      // Data payload of what we are sending back, the url of the signedRequest and a URL where we can access the content after its saved. 
+  const returnData = {
+        signedRequest: data,
+        url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+      };
+      // Send it all back
+      res.json({success:true, data:{returnData}});
+    });
+  }
 
   module.exports = {
     upload
   }
+
+
